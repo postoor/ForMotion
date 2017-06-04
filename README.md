c74c77c9cc1043c3a3046ce7f64cfdf2ffe7d0fb
postoor <postoor@gmail.com>
2017-06-04 23:16:37 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+motion用，簡易腳本
+====
+
+功能：
+- 寄件緩衝
+- 自動刪除舊照片
+
+用法：
+- 將motion.conf的 on\_picture\_save參數修改為
+```
+	on_picture_save picture_save.sh %f
+```
+
+腳本變數說明:
+|變數		|功能				|
+|:---		|:---				|
+|\_DELAY	|隔多久再傳mail(以秒為單位)	|
+|\_DELAY\_FILE	|放置上次傳mail 的時間		|
+|\_PICTURE\_FILE|要傳送的照片路徑		|
+|\_MAIL\_ADDR	|要寄送的email address		|
+|\_PICTURE\_PATH|照片存放路徑			|
+|\_MAX		|照片最大數量			|
+|\_LAVE\_AMOUNT	|當要刪除照片時的保留數量	|
---
+++ picture_save.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+_DELAY=120
+_DELAY_FILE='/tmp/motionTime.txt'
+_PICTURE_FILE=$1
+_MAIL_ADDR='your@email.addr'
+
+_PICTURE_PATH='/your_picture_dir'
+_MAX=40000
+_LAVE_AMOUNT=5000
+
+if [ -f $_DELAY_FILE ]
+then
+	_LAST_TIME=$(cat $_DELAY_FILE)
+else
+	_LAST_TIME=0
+fi
+echo $_LAST_TIME
+
+if [[ $(( $_LAST_TIME + $_DELAY )) < $(date +"%s") ]]
+then
+	echo $(date +"%s") > $_DELAY_FILE
+	mpack -s \"發現變動 $(date +'%c')\" $_PICTURE_PATH $_MAIL_ADDR
+
+fi
+
+count=$(ls $_PICTURE_PATH | wc -l)
+
+if [[ $count -gt $_MAX ]]
+then
+        cd $_PICTURE_PATH
+        ls  | sort -r | tail -n $(($count - $_LAVE_AMOUNT)) | xargs -n 1 rm 
+fi
+
+exit 0
+
